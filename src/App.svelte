f97c265822570e908747d1099952e7a513f26de8
Johann Guayacundo <jjguayacundoa@gmail.com>
2022-02-22 13:56:32 -0500

    :tada: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+import NavBar from "./components/NavBar.svelte";
+
+import ProjectCard from "./components/ProjectCard.svelte";
+
+</script>
+
+<main>
+	<div class="w-11/12 mx-auto pb-10">
+		<NavBar/>
+	</div>
+	<div class="flex flex-col items-center w-10/12 mx-auto">
+		<ProjectCard/>
+	</div>
+	<footer class="bg-black w-screen fixed bottom-0 text-white" >
+		made with love
+	</footer> 
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
---
+++ src/components/ProjectCard.svelte
@@ -0,0 +1,57 @@
+<script>
+    //font-face n shit
+</script>
+
+<div class="inline-flex flex-col shadow-lg rounded-2xl text-sm">
+    <div class="flex flex-row bg-black pl-2">
+        <p class="text-white">Project card - nunuzac.codes</p>
+    </div>
+    <div class="flex gap-6 p-4 items-center">
+        <div class="flex-col hidden md:flex">
+            <img src="../images/GitHub-Mark-120px-plus.png" alt="GitHub">
+        </div>
+        <div class="flex-col">
+            <div class="flex flex-row md:gap-x-2 gap-x-5">
+                <div class="flex gap-1 md:w-28">
+                    <p>📁</p><p class="hidden md:block font-semibold">Source</p>
+                </div>
+                <a class="underline" href="https://github.com/Nunuzac/main-site">https://github.com/Nunuzac/main-site</a>
+            </div>
+            <div class="flex flex-row md:gap-x-2 gap-x-5">
+                <div class="flex gap-1 md:w-28">
+                    <p>📊</p><p class="hidden md:block font-semibold">Status</p>
+                </div>
+                <p>Under construction 🏗</p>
+            </div>
+            <div class="flex flex-row md:gap-x-2 gap-x-5">
+                <div class="flex gap-1 md:w-28">
+                    <p>💬</p><p class="hidden md:block font-semibold">Last Commit</p>
+                </div>
+                <p>:sparkles: added domain records</p>
+            </div>
+            <div class="flex flex-row md:gap-x-2 gap-x-5">
+                <div class="flex gap-1 md:w-28">
+                    <p>👤</p><p class="hidden md:block font-semibold">Commiter</p>
+                </div>
+                <a class="underline" href="mailto:jjguayacundoa@gmail.com">Johann Guayacundo</a>
+            </div>
+            <div class="flex flex-row md:gap-x-2 gap-x-5">
+                <div class="flex gap-1 md:w-28">
+                    <p>📅</p><p class="hidden md:block font-semibold">Commit date</p>
+                </div>
+                <p>2022-02-19T03:16:56</p>
+            </div>
+            <div class="flex flex-row md:gap-x-2 gap-x-5">
+                <div class="flex gap-1 md:w-28">
+                    <p>🛠</p><p class="hidden md:block font-semibold">Tools</p>
+                </div>
+                <div class="grid grid-cols-2 md:grid-cols-3">
+                    <img class="md:justify-self-center" src="https://img.shields.io/badge/node-14.17.0-blue?style=plastic&logo=nodedotjs" alt="node">
+                    <img class="md:justify-self-center" src="https://img.shields.io/badge/svelte-3.0.0-blue?style=plastic&logo=svelte" alt="svelte">
+                    <img class="md:justify-self-center" src="https://img.shields.io/badge/tailwind-3.0.23-blue?style=plastic&logo=tailwindcss" alt="tailwind">
+                </div>
+            </div>
+           
+        </div>
+    </div>
+</div>
